98432f6ec246fb5b3740948db69aae294506a8dd
Rajni Yadav <rajni.yadav1987@gmail.com>
2021-02-05 12:32:41 +0530

    updating code

--- src/com/demo/array/ArraySystemCopyDemo.java
+++ src/com/demo/array/ArraySystemCopyDemo.java
@@ -1,5 +1,6 @@
 package com.demo.array;
 
+enum abc{a, b, c};
 public class ArraySystemCopyDemo {
 	public static void main(String[] args) {
 		char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',
@@ -8,5 +9,8 @@ public class ArraySystemCopyDemo {
         System.arraycopy(copyFrom, 2, copyTo, 0, 7);
         System.out.println(new String(copyTo));
         System.out.println(copyTo);
+        
+        System.out.println(abc.a == abc.a);
+        System.out.println(abc.a.equals(abc.a));
 	}
 }
---
+++ src/com/demo/array/BaseClass.java
@@ -0,0 +1,9 @@
+package com.demo.array;
+
+abstract class BaseClass{
+    protected static void print() {
+    	System.out.println("inside abstract class");
+    }
+    
+    protected abstract void display();
+}
